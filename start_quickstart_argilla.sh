a0f8cf60e98f26e4fbbf3a076d20739fad8c5cf9
RealBigDave912 <115904136+RealBigDave912@users.noreply.github.com>
2023-07-12 19:24:50 -0400

    Add files via upload

---
+++ start_quickstart_argilla.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Starting Elasticsearch"
+elasticsearch 1>/dev/null 2>/dev/null &
+
+echo "Waiting for elasticsearch to start"
+sleep 30
+
+echo "Running database migrations"
+python3.9 -m argilla database migrate
+
+echo "Creating owner user"
+python3.9 -m argilla users create \
+  --first-name "Owner" \
+  --username "$OWNER_USERNAME" \
+  --password "$OWNER_PASSWORD" \
+  --api-key "$OWNER_API_KEY" \
+  --role owner \
+  --workspace "$ARGILLA_WORKSPACE"
+
+echo "Creating admin user"
+python3.9 -m argilla users create \
+  --first-name "Admin" \
+  --username "$ADMIN_USERNAME" \
+  --password "$ADMIN_PASSWORD" \
+  --api-key "$ADMIN_API_KEY" \
+  --role admin \
+  --workspace "$ARGILLA_WORKSPACE"
+
+echo "Creating annotator user"
+python3.9 -m argilla users create \
+  --first-name "Annotator" \
+  --username "$ANNOTATOR_USERNAME" \
+  --password "$ANNOTATOR_PASSWORD" \
+  --role annotator \
+  --workspace "$ARGILLA_WORKSPACE"
+
+# Load data
+python3.9 /load_data.py "$OWNER_API_KEY" "$LOAD_DATASETS" &
+
+# Start Argilla
+echo "Starting Argilla"
+uvicorn argilla:app --host "0.0.0.0"
